6750d79652dee13457aab26b2bc5fe183f5cf337
Altair De Melo <altairdemelo@hotmail.com>
2020-03-06 21:31:49 -0400

    first commit

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php 
+include("includes/header.html"); 
+
+?>
+<title>Login</title>
+
+<body>
+<div class="col-md-4 mx-auto">
+        <div class="card mt-4 text-center"> 
+            <div class="card-header">
+                <h3>Login</h3>
+            </div>
+            <div class="card-body">
+                <form  method="POST">
+                <div class="form-group">
+                    <input type="" name="username" class="form-control" placeholder="Nombre de usuario" autofocus>                   
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" id="" class="form-control" placeholder="Contraseña">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary btn-block" type="submit">Conectar</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
